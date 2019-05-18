2eba663e3e1b3102babe8f2623ac5b487e794650
Nathan Szelag <nszelag@live.com>
2019-05-18 14:07:37 -0400

    added colors and typography. additional styling applied

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ function toggleMenu() {
         $('#js-nav').toggleClass('active');
     })
 }
+function closeMenuOnClick() {
+    let menuLinkActive = document.getElementById('js-nav');
+    $('#js-nav li').on('click', function (e) {
+        if(menuLinkActive.classList.contains('active')){
+            $('#js-nav').toggleClass('active');
+        } 
+    })
+}
 
 function functionHandler() {
     toggleMenu();
+    closeMenuOnClick();
 }
 
 window.onload = functionHandler();
